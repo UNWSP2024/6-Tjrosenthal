28a018c605daa4dd0534a6c5f8186f32e490ffd5
Tjrosenthal <tjrosenthal@students.unwsp.edu>
2025-02-21 08:58:38 -0600

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -5,7 +5,19 @@
 # Write a program that asks the user to enter the total sales for the month.  
 # From this figure, the application should calculate and display the following:
 
-# The amount of county sales tax.
-# The amount of state sales tax.
-# The total sales tax (county plus state)
-# Use at least one function with input and output in this program
+#Tanner Rosenthal
+#2/21/2025
+#Sales and Taxes
+
+def calculate_tax():
+    money_collected = float(input("How much money did you collect?"))
+    state_tax = round(0.05 * money_collected, 2)
+    county_tax = round(0.025 * money_collected, 2)
+    return state_tax, county_tax
+
+
+state, county = calculate_tax()
+total = state + county
+print(f"The state tax amount was ${state:.2f}")
+print(f"The county tax amount was ${county:.2f}")
+print(f"The total amount paid in taxes is ${total:.2f}")
